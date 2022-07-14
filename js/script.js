211a820645eb6c15a8dc908a59afedd2ed7725fc
Natan Ramos <natanramos@MacBook-Pro-de-Natan.local>
2022-07-14 17:51:17 -0300

    portifolio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Desenvolvedor Web","Desenvolvedor Front-End","Um desenvolvedor em busca do full-stack"] ,
+    typeSpeed:90,
+    BackSpeed:40,
+    loop:true
+})
